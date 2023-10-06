14c6dfb0eaab71ac8633e5259257b2cb4a6d6b43
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-10-06 11:48:06 +0200

    completes intro section

--- components/introduction.tsx
+++ components/introduction.tsx
@@ -4,6 +4,10 @@ import Image from "next/image";
 import ProfileImage from "../public/sayedaPicture.jpeg";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import { HiDownload } from "react-icons/hi";
+import { BsLinkedin } from "react-icons/bs";
+import { BsArrowRight } from "react-icons/bs";
+import { FaGithubSquare } from "react-icons/fa";
 
 const Introduction = () => {
   return (
@@ -52,14 +56,47 @@ const Introduction = () => {
         responsive digital interfaces. I craft seamless user experiences to
         bring websites to life.
       </motion.p>
-      <div className="bg-gray-900 text-white px-2 py-2 rounded-full ">
-        <Link href="#contact">Contact Me</Link>
-      </div>
-      <div className="bg-white px-2 py-2 rounded-full">
-        <a href="" className="">
+      <motion.div
+        className=" flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-6 py-2 flex items-center justify-center gap-2 
+          rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+        >
+          Contact Me
+          <BsArrowRight className="opacity-60 group-hover:translate-x-1 transition" />
+        </Link>
+
+        <a
+          href="/CV.pdf download"
+          className=" group bg-white px-6 py-2 rounded-full flex items-center justify-center gap-2 outline-none focus:scale-110 hover:scale-110 active:-scale-105 transition border border-black/10"
+        >
           Download Cv
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
-      </div>
+
+        <a
+          href="https://linkedin.com"
+          target="_blank"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-[1.15] hover:text-gray-950  active:-scale-[1.15] transition border border-black/10"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          href="https://github.com"
+          target="_blank"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.25rem] rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:-scale-105 transition border border-black/10"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   );
 };
