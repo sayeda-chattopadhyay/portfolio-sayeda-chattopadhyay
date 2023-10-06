d1a50bdb402fa8958ceb5ca16659532673dd0ac9
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-10-06 11:59:22 +0200

    addssection  divider

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Introduction from "@/components/introduction";
+import SectionDivider from "@/components/section-divider";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Introduction />
+      <SectionDivider />
     </main>
   );
 }
