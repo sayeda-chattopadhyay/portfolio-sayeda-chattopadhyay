b8df7a2150ca441aa133a2b5d0b2bc7bbb7ca50c
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-10-10 23:06:41 +0200

    adds skills section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Introduction from "@/components/introduction";
 import SectionDivider from "@/components/section-divider";
 import About from "@/components/about";
 import Projects from "@/components/projects";
+import Skills from "@/components/skills"
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <SectionDivider />
       <About />
       <Projects/>
+      <Skills/>
     </main>
   );
 }
---
+++ components/skills.tsx
@@ -0,0 +1,48 @@
+"use client";
+import SectionHeading from "@/components/section-heading";
+import { skillsData } from "@/lib/data";
+import { motion } from "framer-motion";
+
+const animationVariants = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  animate: (index: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.05 * index,
+    },
+  }),
+};
+
+const Skills = () => {
+  return (
+    <section
+      className="mb-28 max-w-[53rem] text-center sm:mb-40 scroll-mt-28"
+      id="skills"
+    >
+      <SectionHeading>My skills</SectionHeading>
+      <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
+        {skillsData.map((skill, index) => (
+          <motion.li
+            key={index}
+            className="bg-white border border-black/[0.3] px-3 py-2 rounded-lg"
+            variants={animationVariants}
+            initial="initial"
+            whileInView="animate"
+            viewport={{
+              once: true,
+            }}
+            custom={index}
+          >
+            {skill}
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Skills;
