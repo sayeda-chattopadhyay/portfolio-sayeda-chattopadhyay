5de563fa7fe00f73fbc3455a3e97009855deca95
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-10-12 01:13:20 +0200

    change card style

--- components/project.tsx
+++ components/project.tsx
@@ -35,12 +35,13 @@ export default function Project({
       className="group mb-3 sm:mb-8 last:mb-0"
       onClick={() => setIsOpen(!isOpen)}
     >
-      <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:flex dark:bg-white/10 dark:hover:bg-white/20">
+      <section className="bg-gray-100 max-w-[60rem] px-4 py-4 border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:min-h-[20rem] hover:bg-gray-200 transition sm:flex dark:bg-white/10 dark:hover:bg-white/20">
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/80">
             {description}
           </p>
+          <hr className="my-6 mx-6" />
           <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
             {tags.map((tag, index) => (
               <li
@@ -51,6 +52,21 @@ export default function Project({
               </li>
             ))}
           </ul>
+          <hr className="my-6 mx-6" />
+          <div className="flex justify-center gap-4">
+            <a
+              href={liveUrl}
+              className="border border-black/10 px-8 py-2 rounded-xl dark:border-white/20"
+            >
+              Live
+            </a>
+            <a
+              href={gitHubUrl}
+              className="border border-black/10 px-8 py-2 rounded-xl dark:border-white/20"
+            >
+              Github
+            </a>
+          </div>
         </div>
 
         <div className="sm:w-[50%]">
@@ -62,21 +78,6 @@ export default function Project({
           />
         </div>
       </section>
-      {isOpen && (
-        <motion.div
-          className=" w-100 h-100 p-3 border border-black/10 
-           initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}"
-        >
-          <h3 className="text-2xl font-semibold">{title}</h3>
-          <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/80">
-            {description}
-          </p>
-          <a href={liveUrl}>Live</a>
-          <a href={gitHubUrl}>Github</a>
-        </motion.div>
-      )}
     </motion.div>
   );
 }
