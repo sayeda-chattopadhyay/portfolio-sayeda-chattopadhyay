5f34a87920dcdc3f5fefa23c247e7cfcdb3ac749
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-10-13 14:28:59 +0200

    changes project card style

--- components/project.tsx
+++ components/project.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef, useState } from "react";
+import { useRef } from "react";
 import { projectsData } from "@/lib/data";
 import Image from "next/image";
 import { motion, useScroll, useTransform } from "framer-motion";
@@ -12,11 +12,7 @@ export default function Project({
   description,
   tags,
   imageUrl,
-  gitHubUrl,
-  liveUrl,
 }: ProjectProps) {
-  const [isOpen, setIsOpen] = useState(false);
-
   const ref = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
     target: ref,
@@ -33,15 +29,13 @@ export default function Project({
         opacity: opacityProgess,
       }}
       className="group mb-3 sm:mb-8 last:mb-0"
-      onClick={() => setIsOpen(!isOpen)}
     >
-      <section className="bg-gray-100 max-w-[60rem] px-4 py-4 border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:min-h-[20rem] hover:bg-gray-200 transition sm:flex dark:bg-white/10 dark:hover:bg-white/20">
+      <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
-          <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/80">
+          <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
             {description}
           </p>
-          <hr className="my-6 mx-6" />
           <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
             {tags.map((tag, index) => (
               <li
@@ -52,102 +46,26 @@ export default function Project({
               </li>
             ))}
           </ul>
-          <hr className="my-6 mx-6" />
-          <div className="flex justify-center gap-4">
-            <a
-              href={liveUrl}
-              className="border border-black/10 px-8 py-2 rounded-xl dark:border-white/20"
-            >
-              Live
-            </a>
-            <a
-              href={gitHubUrl}
-              className="border border-black/10 px-8 py-2 rounded-xl dark:border-white/20"
-            >
-              Github
-            </a>
-          </div>
         </div>
 
-        <div className="sm:w-[50%]">
-          <Image
-            src={imageUrl}
-            alt="Project I worked on"
-            quality={95}
-            className="w-full sm:h-full object-cover rounded-t-lg shadow-2xl transition group-hover:scale-[1.04] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2 group-even:right-[initial] group-even:-left-40"
-          />
-        </div>
+        <Image
+          src={imageUrl}
+          alt="Project I worked on"
+          quality={95}
+          className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
+        transition 
+        group-hover:scale-[1.04]
+        group-hover:-translate-x-3
+        group-hover:translate-y-3
+        group-hover:-rotate-2
+
+        group-even:group-hover:translate-x-3
+        group-even:group-hover:translate-y-3
+        group-even:group-hover:rotate-2
+
+        group-even:right-[initial] group-even:-left-40"
+        />
       </section>
     </motion.div>
   );
 }
-
-// "use client";
-
-// import { useRef } from "react";
-// import { projectsData } from "@/lib/data";
-// import Image from "next/image";
-// import { motion, useScroll, useTransform } from "framer-motion";
-
-// type ProjectProps = (typeof projectsData)[number];
-
-// export default function Project({
-//   title,
-//   description,
-//   tags,
-//   imageUrl,
-// }: ProjectProps) {
-//   const ref = useRef<HTMLDivElement>(null);
-//   const { scrollYProgress } = useScroll({
-//     target: ref,
-//     offset: ["0 1", "1.33 1"],
-//   });
-//   const scaleProgess = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
-//   const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
-
-//   return (
-//     <motion.div
-//       ref={ref}
-//       style={{
-//         scale: scaleProgess,
-//         opacity: opacityProgess,
-//       }}
-//       className="group mb-3 sm:mb-8 last:mb-0"
-//     >
-//       <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8">
-//         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
-//           <h3 className="text-2xl font-semibold">{title}</h3>
-//           <p className="mt-2 leading-relaxed text-gray-700 ">{description}</p>
-//           <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
-//             {tags.map((tag, index) => (
-//               <li
-//                 className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
-//                 key={index}
-//               >
-//                 {tag}
-//               </li>
-//             ))}
-//           </ul>
-//         </div>
-
-//         <Image
-//           src={imageUrl}
-//           alt="Project I worked on"
-//           quality={95}
-//           className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
-//         transition
-//         group-hover:scale-[1.04]
-//         group-hover:-translate-x-3
-//         group-hover:translate-y-3
-//         group-hover:-rotate-2
-
-//         group-even:group-hover:translate-x-3
-//         group-even:group-hover:translate-y-3
-//         group-even:group-hover:rotate-2
-
-//         group-even:right-[initial] group-even:-left-40"
-//         />
-//       </section>
-//     </motion.div>
-//   );
-// }
