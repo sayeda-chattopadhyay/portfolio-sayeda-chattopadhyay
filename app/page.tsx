666ed19c08d798060f2eeddfbc64924258cb1826
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-10-16 14:28:46 +0200

    removes divider

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Introduction from "@/components/introduction";
-import SectionDivider from "@/components/section-divider";
+
 import About from "@/components/about";
 import Projects from "@/components/projects";
 import Skills from "@/components/skills";
@@ -8,7 +8,6 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Introduction />
-      <SectionDivider />
       <About />
       <Projects />
       <Skills />
