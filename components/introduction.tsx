fe9ed8d1859a4fb615d2de2fe6d3dd9b6362da6d
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-11-11 09:53:36 +0100

    changes motion duration

--- components/introduction.tsx
+++ components/introduction.tsx
@@ -12,7 +12,7 @@ const Introduction = () => {
       className="mb-28 sm:mb-0 scroll-mt-[100rem]"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      transition={{ delay: 0.5, duration: 3.5 }}
+      transition={{ delay: 0.3, duration: 0.5 }}
     >
       <div className="px-8 py-4 shadow-sm w-full max-w-[100rem] flex flex-col  gap-14 mt-10 items-center sm:flex-row sm:justify-between sm:gap-40 sm:mt-20">
         {/* text */}
@@ -49,19 +49,19 @@ const Introduction = () => {
           </motion.div>
         </div>
         {/* image */}
-        <motion.div
+        {/* <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 1.0, duration: 2.0 }}
-        >
-          <Image
-            src={ProfileImage}
-            alt="profile Image"
-            quality="95"
-            priority={true}
-            className="rounded-lg object-cover "
-          />
-        </motion.div>
+        > */}
+        <Image
+          src={ProfileImage}
+          alt="profile Image"
+          quality="95"
+          priority={true}
+          className="rounded-lg object-cover "
+        />
+        {/* </motion.div> */}
       </div>
     </motion.section>
   );
